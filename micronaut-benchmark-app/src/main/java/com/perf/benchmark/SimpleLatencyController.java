44777294bab8d7bac463ba68273f027ab7229842
Ankur Kumar <ankurkumar78@gmail.com>
2021-04-06 23:17:49 -0700

    added ab benchmark results

--- micronaut-benchmark-app/src/main/java/com/perf/benchmark/SimpleLatencyController.java
+++ micronaut-benchmark-app/src/main/java/com/perf/benchmark/SimpleLatencyController.java
@@ -1,22 +1,28 @@
 package com.perf.benchmark;
 
-import io.micronaut.http.MediaType;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
+import io.micronaut.validation.Validated;
 
+@Validated
 @Controller("/latency")
 public class SimpleLatencyController {
-    @Get // <2>
-    @Produces(MediaType.TEXT_PLAIN)
-    public String index(HttpRequest<?> request) {
+	
+
+    @Get
+    @Produces(MediaType.APPLICATION_JSON)
+    public HttpResponse<?> index(HttpRequest<?> request) {
     	String delay = request.getParameters().get("delay");
     	try {
     		Thread.sleep(delay!=null?Integer.parseInt(delay):0);
     	} catch (Exception e) { 
     		e.printStackTrace();
     	}
-        return "Hello World with Latency " + delay; //
-    }
+        return HttpResponse.status(HttpStatus.OK).body("Hello World with Delay:"+delay);
+    }   
 }
